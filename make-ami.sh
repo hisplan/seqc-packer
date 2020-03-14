10c7ecbb79ad234b4b1ba55b1d1e38dd93cab4a5
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-03-13 21:32:41 -0400

    Support multiple regions

--- make-ami.sh
+++ make-ami.sh
@@ -1,10 +1,34 @@
 #!/bin/bash -e
 
+usage()
+{
+cat << EOF
+USAGE: `basename $0` [options]
+    -r  destination AMI region (e.g. us-east-1, eu-west-1, ...)
+EOF
+}
+
+while getopts "r:h" OPTION
+do
+    case $OPTION in
+        r) ami_region=$OPTARG ;;
+        h) usage; exit 1 ;;
+        *) usage; exit 1 ;;
+    esac
+done
+
+if [ -z "$ami_region" ]
+then
+    usage
+    exit 1
+fi
+
 # run packer
 # send stdout to build.log
 packer build \
     -only=amazon-ebs \
     -machine-readable \
+    -var-file=config.${ami_region}.json \
     seqc.packer | tee build.log
 
 # exit if packer failed
@@ -25,7 +49,10 @@ then
     exit 1
 fi
 
+# get ami metadata
+image_meta=`aws ec2 describe-images --image-ids ${ami_id} --region ${ami_region}`
+
 # get ebs snapshot id
-ebs_snapshot_id=`aws ec2 describe-images --image-ids ${ami_id} | jq -r .Images[].BlockDeviceMappings[0].Ebs.SnapshotId`
+ebs_snapshot_id=`echo ${image_meta} | python -c "import json; import sys; data=json.load(sys.stdin); print(data['Images'][0]['BlockDeviceMappings'][0]['Ebs']['SnapshotId'])"`
 
 echo "EBS Snapshot ID: ${ebs_snapshot_id}"
