14a3478d119e82e60c72eb4785beac79fedc7b3c
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-07-17 20:34:24 -0400

    Use Python 3.7.6, Miniconda 4.8.2

--- scripts/install-miniconda.sh
+++ scripts/install-miniconda.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
-# this version comes with python 3.6.5
-miniconda_version="4.5.1"
+# this version comes with Python 3.7.6
+# https://repo.anaconda.com/miniconda/
+# https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh
+miniconda_version="4.8.2"
 
 cd /tmp
-curl -O https://repo.anaconda.com/miniconda/Miniconda3-${miniconda_version}-Linux-x86_64.sh
-sudo bash Miniconda3-${miniconda_version}-Linux-x86_64.sh -b -p /opt/conda
-rm -rf Miniconda3-${miniconda_version}-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-py37_${miniconda_version}-Linux-x86_64.sh
+sudo bash Miniconda3-py37_${miniconda_version}-Linux-x86_64.sh -b -p /opt/conda
+rm -rf Miniconda3-py37_${miniconda_version}-Linux-x86_64.sh
 sudo ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
 echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
 echo "PATH=/opt/conda/bin:$PATH" >> ~/.bashrc
